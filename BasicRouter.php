220cd952326b33a3c2103442485f57dc58faa160
Branko Stepanovic <banestep@gmail.com>
2020-03-12 13:13:33 +0100

    Adding index.php and a basic router

---
+++ BasicRouter.php
@@ -0,0 +1,44 @@
+<?php
+
+class BasicRouter 
+{
+    function __construct() 
+    {
+        $uri    = trim($_SERVER['REQUEST_URI'], '/');
+        $urray  = explode('/', $uri);
+
+        if(count($urray) > 0)
+        {
+            $class_name = $this->toCamelCase($urray[0]);
+            if(class_exists($class_name))
+            {
+                $routebject = new $class_name($urray[1]);
+            }
+            else
+            {
+                echo "Bad route or no paramter. It's imperfect routing, I know...";
+            }
+        }
+        else
+        {
+            echo 'Hardcoding home page to save time.';
+        }
+
+        //var_dump($urray);
+    }
+    
+    private function toCamelCase($string)
+    {
+      $result = strtolower($string);
+          
+      preg_match_all('/_[a-z]/', $result, $matches);
+  
+      foreach($matches[0] as $match)
+      {
+          $c = str_replace('_', '', strtoupper($match));
+          $result = str_replace($match, $c, $result);
+      }
+  
+      return $result;
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/BasicRouter.php';
+
+$route = new BasicRouter;
