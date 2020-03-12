6a3989ef2b03cf4fad3deff1df51f0ab30d00bb3
Branko Stepanovic <banestep@gmail.com>
2020-03-12 13:21:07 +0100

    Adding db config to index.php

--- index.php
+++ index.php
@@ -4,4 +4,11 @@ require __DIR__ . '/BasicRouter.php';
 require __DIR__ . '/Students.php';
 require __DIR__ . '/Student.php';
 
+/* config */
+define('DBHOST', '');
+define('DBNAME', '');
+define('DBUSER', '');
+define('DBPASS', '');
+/* end of config */
+
 $route = new BasicRouter;
